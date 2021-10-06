63c2e1467df5e51e9dc8524dae8f6c970a4e6d25
Sreenath Rangana <sreenath@zelarsoft.com>
2021-10-06 22:16:14 +0530

    demo tf gcp

---
+++ GCP-TF/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-328213-e4bd745d8e1a.json")
+
+  project = "terraform-328213"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
